8a7c90bb2f8763311471ad914da5b345ec30b6c1
John Lynch <johnrlynch@gmail.com>
2013-09-21 18:29:54 -0700

    Update visualizer.rb

--- visualizer.rb
+++ visualizer.rb
@@ -1,13 +1,50 @@
+require 'curses'
+
 class Visualizer
-    
+
+    @@count = 1
     def self.ascii(grid)
-        puts "Its Alive! (ASCII)"
-        (0..grid.cols).each do |col|
-            put col
-            (0..grid.rows).each do |row|
-              puts (grid.cell_at(row,col).alive? ? "*" : ".")
+        puts "#{grid.rows}x#{grid.cols}:#{@@count} ==========================="
+        (0...grid.cols).each do |col|
+            (0...grid.rows).each do |row|
+              print (grid.cell_at(row,col).alive? ? "*" : ".")
             end
-            put "\n"
+         print "\n"
         end
+        puts
+        @@count +=1
+    end
+
+    def self.init_curses(grid)
+        Curses.init_screen
+        window = Curses::Window.new grid.rows, grid.cols, 0, 0
+        window.nodelay = true
+        window
+    end
+
+    def self.curses(window, grid)
+        (0...grid.cols).each do |col|
+            (0...grid.rows).each do |row|
+              window.setpos row, col
+              if grid.cell_at(row,col).alive?
+                  window.addstr "*"
+              else
+                  window.addstr "."
+              end
+            end
+        end
+        window.refresh
+        @@count +=1
+
+    end
+
+    def self.alive_neighbors_count(grid)
+        (0...grid.cols).each do |col|
+            (0...grid.rows).each do |row|
+              print grid.cell_at(row,col).alive_neighbors_count
+            end
+         print "\n"
+        end
+        puts
     end
 end
