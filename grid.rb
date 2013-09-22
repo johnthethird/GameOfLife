e25bc602afb84567a77e65a2b29d4898627666ed
John Lynch <johnrlynch@gmail.com>
2013-09-21 18:30:39 -0700

    Update grid.rb

--- grid.rb
+++ grid.rb
@@ -1,83 +1,96 @@
-require 'cell'
+require_relative 'cell'
 require 'matrix'
 
 class Grid
-   
+
   attr_accessor :grid, :rows, :cols
-   
-   def initailize(seed_file_path)
+
+   def initialize(seed_file_path)
      seed_data = load_seed(seed_file_path)
      @grid = grid_from_seed(seed_data)
    end
-   
+
+   def cell_at(row,col)
+       @grid.element(row, col) if row >= 0 && col >= 0
+   end
+
+   def get_neighbors(row,col)
+        cells = []
+        (-1..1).each do |offset_row|
+            (-1..1).each do |offset_col|
+              cells << cell_at(row+offset_row,col+offset_col) unless offset_row == 0 && offset_col == 0
+            end
+        end
+        cells.compact
+    end
+
+
    def load_seed(seed_file_path)
        seed_data = []
        @cols = 0
        line_idx = 0
        File.open(seed_file_path, 'r').each_line do |line|
          my_line = []
-         num_line_chars = 1
+         num_line_chars = 0
          char_idx = 0
          line.strip.each_char do |char|
-           my_line << Cell.new(self, alive_or_dead?(char), char_idx, line_idx)
+             puts "#{char}: #{alive?(char)}"
+           my_line << Cell.new(self, alive?(char), line_idx, char_idx)
            num_line_chars += 1
            @cols = num_line_chars if num_line_chars > @cols
            char_idx += 1
          end
-         @rows = line_idx
+         @rows = line_idx + 1
          line_idx += 1
          seed_data << my_line
        end
-       seed_data = self.pad_seed_data(seed_data)
+       self.pad_seed_data(seed_data)
    end
-   
+
    def pad_seed_data(seed_data)
        seed_data.each_with_index do |row, line_idx|
          if row.size < @cols
             row_size_diff = @cols - row.size
             char_idx = row.size
             row_size_diff.times do
-              row <<  Cell.new(self, :dead, char_idx, line_idx)
+                puts "padding #{line_idx} #{char_idx}"
+              row <<  Cell.new(self, false, line_idx, char_idx)
               char_idx += 1
             end
          end
        end
-   end
-   
-   def alive_or_dead?(char)
-      if char == '.'
-        :dead 
-      else
-        :live 
-      end
+       seed_data
    end
 
-   
+   def alive?(char)
+      char != '.'
+   end
+
    def grid_from_seed(seed_data)
       Matrix.rows(seed_data)
    end
-   
-   def cell_at(row, col)
-       # if row, col is outside the grid bounds, return a dead cell
-       return new Cell(self, :dead, row, col) if row > rows || col > cols         
-   end
-   
+
    def next_turn!
-       (0..rows).each do |row|
-           (0..cols).each do |col|
-               cell_at(row,col).next!
-           end
-       end
+     calculate_next!
+     update_state!
    end
-   
+
    private
-   
-   def alive_or_dead?(char)
-      if char == '.'
-        :alive
-      else
-        :dead
-      end
+
+   def calculate_next!
+     (0...rows).each do |row|
+         (0...cols).each do |col|
+             cell_at(row,col).calculate_next!
+         end
+     end
    end
-   
+
+   def update_state!
+     (0...rows).each do |row|
+         (0...cols).each do |col|
+             cell_at(row,col).update_state!
+         end
+     end
+   end
+
 end
