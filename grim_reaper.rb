68d65997ac339588da83ceba96be8775cbefc498
John Lynch <johnrlynch@gmail.com>
2013-09-21 18:31:11 -0700

    Update grim_reaper.rb

--- grim_reaper.rb
+++ grim_reaper.rb
@@ -1,21 +1,22 @@
-require 'grid'
-require 'cell'
-
+require_relative 'grid'
+require_relative 'visualizer'
+require 'debugger'
 class GrimReaper
-    
-    SEED_FILE = ARGV[0]
-    
+
+    SEED_FILE = ARGV[0] || "seed_file.txt"
+
     def self.start_game!
       @@grid = Grid.new(SEED_FILE)
+      @@window = Visualizer.init_curses(@@grid)
       while(true)
-        next_turn()
+        Visualizer.curses(@@window, @@grid)
+        #Visualizer.ascii(@@grid)
+        #Visualizer.alive_neighbors_count(@@grid)
+        @@grid.next_turn!
+        sleep(0.25)
       end
     end
-    
-    private
 
-    def self.next_turn
-      @@grid.next_turn!
-    end
-    
 end
+
+GrimReaper.start_game!
